67f4ef4887361c856d87172cb01cd25797cf0eff
Kgotso-20 <cmalatji0@gmail.com>
2023-07-28 08:30:59 -0700

    C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 int check_pal(char *s, int i, int len);
-
 int _strlen_recursion(char *s);
 
 /**
- *is_palindrome - checks if a string is a palindrome
- *@s: string to reverse
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
  *
- *Return: 1 if it is, 0 it's not
+ * Return: 1 if it is, 0 it's not
  */
-
 int is_palindrome(char *s)
 {
 	if (*s == 0)
@@ -19,12 +17,11 @@ int is_palindrome(char *s)
 }
 
 /**
- *_strlen_recursion - returns the length of a string
- *@s: string to calculate the length of
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
  *
- *Return: length of the string
+ * Return: length of the string
  */
-
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
@@ -33,14 +30,13 @@ int _strlen_recursion(char *s)
 }
 
 /**
- *check_pal - checks the characters recursively for palindrome
- *@s: string to check
- *@i: iterator
- *@len: length of the string
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
  *
- *Return: 1 if palindrome, 0 if not
+ * Return: 1 if palindrome, 0 if not
  */
-
 int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
@@ -49,3 +45,4 @@ int check_pal(char *s, int i, int len)
 		return (1);
 	return (check_pal(s, i + 1, len - 1));
 }
+
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *wildcmp - Compare strings
- *@s1: pointer to string params
- *@s2: pointer to string params
- *Return: 0
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
 
 int wildcmp(char *s1, char *s2)
@@ -17,6 +17,7 @@ int wildcmp(char *s1, char *s2)
 		}
 		return (*s2 == '\0');
 	}
+
 	if (*s2 == '*')
 	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
@@ -27,3 +28,4 @@ int wildcmp(char *s1, char *s2)
 	}
 	return (0);
 }
+
